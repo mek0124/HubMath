9162390801ecfd4af6d76e77854475e49266135d
Mekasu <mekasurenae@gmail.com>
2025-05-05 15:53:43 -0500

    built landing screen

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,9 +1,35 @@
-
+import { Link } from 'react-router-dom';
 
 
 export default function Landing() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-[100vh]">
+      <div className="flex flex-col items-center justify-center w-[50%]">
+        <h1 className="font-bold text-fontColor text-4xl text-center w-full">
+          Welcome To Hub Math!
+        </h1>
+
+        <h3 className="italic text-fontColor text-md text-center w-full">
+          A Math Game For Ages 3+
+        </h3>
+      </div>
+
+      <div className="flex flex-row items-center justify-evenly w-[79%] h-52">
+        <Link to="/auth/signup"
+          className="font-bold text-xl text-fontColor text-center border-2 border-tertiary w-60 p-1 rounded-xl transform transition duration-300 ease-in-out hover:bg-tertiary hover:shadow-lg hover:scale-105">
+            Sign Up
+        </Link>
+
+        <Link to="/auth/login"
+          className="font-bold text-xl text-fontColor text-center border-2 border-tertiary w-60 p-1 rounded-xl transform transition duration-300 ease-in-out hover:bg-tertiary hover:shadow-lg hover:scale-105">
+            Login
+        </Link>
+
+        <Link to="/game-menu"
+          className="font-bold text-xl text-fontColor text-center border-2 border-tertiary w-60 p-1 rounded-xl transform transition duration-300 ease-in-out hover:bg-tertiary hover:shadow-lg hover:scale-105">
+            Play As Guest
+        </Link>
+      </div>
     </div>
   );
 };
