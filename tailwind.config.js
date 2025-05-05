5c1932cc1bb2712b561fafcffeb432c5cedfec17
Mekasu <mekasurenae@gmail.com>
2025-05-05 03:28:05 -0500

    configured tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'rgb(0,22,45)',
+        secondary: 'rgb(0,38,80)',
+        tertiary: 'rgb(33,84,141)',
+        accent: 'rgb(94,147,207)',
+        fg: 'rgb(177,203,231)',
+      },
+    },
+    fontFamily: {
+      tektur: ["Tektur", "sans-serif"],
+      hennyPenny: ["Henny Penny", "sans-serif"],
+    },
+  },
+  plugins: [],
+}
+
