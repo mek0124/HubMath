46d2d89d8fd70786bd8929777367b1715948ad2a
Mekasu <mekasurenae@gmail.com>
2025-05-07 17:59:18 -0500

    added screens into routes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Header from './components/header';
 import Landing from './pages/landing';
 import SignUp from './pages/auth/signup';
 import Login from './pages/auth/login';
+import GameMenu from "./pages/game/menu";
+import GamePlay from "./pages/game/gamePlay";
 
 
 export default function App() {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-[100vh]">
+    <div className="flex flex-col items-center justify-center w-full min-h-screen">
       <Header />
 
       <Routes>
@@ -17,6 +19,10 @@ export default function App() {
         <Route path="/auth">
           <Route path="sign-up" element={<SignUp />} />
           <Route path="login" element={<Login />} />
+        </Route>
+
+        <Route path="/game">
+          <Route path="menu" element={<GameMenu />} />
         </Route>
       </Routes>
     </div>
